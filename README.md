7e65921d2742bbd68eb7aee103cbafc8b27ad4f2
Henryk Paluch <hpaluch@seznam.cz>
2020-05-10 17:14:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# PIC32MX UART LED PWM
+
+Introductory project for [Microstick II][PIC Microstick II] 
+with PIC32MX.
+
+It *will* allow controlling LED brightness via commands
+on RS232/UART port.
+
+WARNING! Work in progress. Currently it flip-flops two levels
+of brightness of onboard LED D6 each second.
+
+
+Used peripherals:
+- on-chip oscillator 8MHz 
+- TMR2 - 1ms interval, PR2=62, clock source for PWM (OCMP1)
+- OCMP1 - PWM output to on-board LED D6
+
+
+More information on PWM is available on [PIC32 Output Compare data sheet - PDF][PIC32 Output Compare].
+
+# Requirements
+
+## Hardware requirements
+
+* [Microstick II][PIC Microstick II]  demo board
+* [PIC32MX250F128B SPDIP][PIC32MX250F128B] inserted into socket U5
+  (included with board, should be default)
+* programming pins switch S1 in position `A`
+
+## Software requirements
+
+* [XC32 compiler][XC compilers] - tested version v2.30
+* [MPLAB X IDE][MPLAB X IDE] - tested version v5.25
+  - installed  [MPLAB Harmony Configurator 3 Plugin][Harmony] - tested
+    version 3.4.2
+  - uses only `csp` component from Harmony Framework
+
+[PIC32 Output Compare]: http://ww1.microchip.com/downloads/en/devicedoc/61111e.pdf
+[Harmony]: https://www.microchip.com/mplab/mplab-harmony
+[XC compilers]: https://www.microchip.com/mplab/compilers
+[MPLAB X IDE]: https://www.microchip.com/mplab/mplab-x-ide
+[PIC32MX250F128B]: https://www.microchip.com/wwwproducts/en/PIC32MX250F128B
+[PIC Microstick II]: https://www.microchip.com/DevelopmentTools/ProductDetails/dm330013-2
---
+++ firmware/src/main.c
@@ -0,0 +1,88 @@
+/*******************************************************************************
+  Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This file contains the "main" function for a project.
+
+  Description:
+    This file contains the "main" function for a project.  The
+    "main" function calls the "SYS_Initialize" function to initialize the state
+    machines of all modules in the system
+ *******************************************************************************/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <string.h>                     // strlen(3)
+#include <stddef.h>                     // Defines NULL
+#include <stdbool.h>                    // Defines true
+#include <stdlib.h>                     // Defines EXIT_FAILURE
+#include "definitions.h"                // SYS function prototypes
+
+
+
+//static const char *MSG_INIT="Init...\r\n";
+static const char *MSG_LOOP="Entering main loop...\r\n";
+
+static volatile uint16_t counter2 = 0;
+
+//TMR2 for PWM, called each 1ms
+void TIMER2_EventHandler(uint32_t status, uintptr_t context)
+{
+    counter2++;
+    // each second change brightness of LED
+    if (counter2 == 1000){
+        OC1RS = 8000;
+    }
+    if (counter2 == 2000){
+        OC1RS = 2000;
+        counter2 = 0;
+    }
+} 
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Main Entry Point
+// *****************************************************************************
+// *****************************************************************************
+
+int main ( void )
+{
+    /* Initialize all modules */
+    SYS_Initialize ( NULL );
+
+    // commented - must wait after write
+    //UART2_Write(MSG_INIT,strlen(MSG_INIT));
+    
+    OCMP1_Enable();
+    
+    TMR2_CallbackRegister(TIMER2_EventHandler,(uintptr_t)NULL);     
+    TMR2_Start();
+
+    UART2_Write((void*)MSG_LOOP,strlen(MSG_LOOP));
+    
+    while ( true )
+    {
+        /* Maintain state machines of all polled MPLAB Harmony modules. */
+        SYS_Tasks ( );
+    }
+
+    /* Execution should not come here during normal operation */
+
+    return ( EXIT_FAILURE );
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
